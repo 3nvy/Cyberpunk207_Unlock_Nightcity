02bc19a5218a3eeb2dbc310aa5dc2cb2fd574ce5
Renato Pestana <renatompestana@gmail.com>
2021-01-17 19:14:21 +0000

    Added smuggler

--- bin/x64/plugins/cyber_engine_tweaks/mods/Unlock NightCity/init.lua
+++ bin/x64/plugins/cyber_engine_tweaks/mods/Unlock NightCity/init.lua
@@ -1,6 +1,9 @@
 -- Change Slot Cooldown Time Here (in minutes)
 slotCoolDownTime = 1440
 
+-- Change Smuggling Cost
+smugglingCost = 50000
+
 
 -- Mod Code
 packages = {}
@@ -11,6 +14,7 @@ packages.TeleportManager = require(rootPath .. "teleports.teleports")
 packages.DoorManager = require(rootPath .. "doors.doors")
 packages.DeviceManager = require(rootPath .. "devices.devices")
 packages.SlotMachineManager = require(rootPath .. "slotmachine.slotmachine")
+packages.SmugglerManager = require(rootPath .. "smuggler.smuggler")
 
 
 local tt = Game.GetTargetingSystem()
@@ -24,6 +28,8 @@ end)
 registerForEvent("onUpdate", function()
 	
 	local stopAfter = false
+
+	packages.SmugglerManager.UpdateSmugglerWindow()
 
 	if (ImGui.IsKeyPressed(0x46, false)) then -- Press F
 
@@ -47,6 +53,42 @@ registerForEvent("onUpdate", function()
 		stopAfter = packages.DoorManager.CheckAllowedDoor(object)
 		if stopAfter then return end
 
+
+		-- Check for Smuggler
+		stopAfter = packages.SmugglerManager.CheckSmuggler(object, smugglingCost)
+		if stopAfter then return end
+
+
 	end
 
+end)
+
+registerForEvent("onDraw", function()
+	ImGui.PushStyleColor(ImGuiCol.PopupBg, 0.21, 0.08, 0.08, 0.85)
+	ImGui.PushStyleColor(ImGuiCol.Border, 0.4, 0.17, 0.12, 1)
+	ImGui.PushStyleColor(ImGuiCol.Separator, 0.4, 0.17, 0.12, 1)
+
+	if (drawSmuggleWindow) then
+		ImGui.BeginTooltip()
+		ImGui.SetWindowFontScale(1.6)
+		ImGui.Spacing()
+			ImGui.TextColored(0.45, 1, 0.5, 1, "[Warning] Getting out of the Watson District during Lockdown may break some quests.")
+			ImGui.Spacing()
+			ImGui.Separator()
+			ImGui.Spacing()
+			ImGui.TextColored(0.2, 1, 1, 1, "Sure, I can smuggle you out of the Watson District, but it will cost you.")
+			ImGui.Spacing()
+			ImGui.TextColored(0.2, 1, 1, 1, "Say, 50.000 eddies? I need it to be worth the risk for me!")
+			ImGui.Spacing()
+			ImGui.Separator()
+			ImGui.Spacing()
+			ImGui.TextColored(0.98, 0.85, 0.25, 1, "[F] Sure, just get me out of here already")
+			ImGui.Spacing()
+			ImGui.Spacing()
+			ImGui.TextColored(0.98, 0.85, 0.25, 1, "[Look Away] Nah, maybe another time.")
+			ImGui.Spacing()
+		ImGui.EndTooltip()
+	end
+
+	ImGui.PopStyleColor(3)
 end)
---
+++ bin/x64/plugins/cyber_engine_tweaks/mods/Unlock NightCity/smuggler/smuggler.lua
@@ -0,0 +1,45 @@
+local SmugglerManager = {}
+
+
+local tt = Game.GetTargetingSystem()
+local ts = Game.GetTransactionSystem()
+local tp = Game.GetTeleportationFacility()
+local as = Game.GetActivityLogSystem()
+
+local moneyID = ItemID.new(TweakDBID.new("Items.money"))
+
+drawSmuggleWindow = false
+
+function SmugglerManager.UpdateSmugglerWindow()
+    local object = tt:GetLookAtObject(Game.GetPlayer(), false, false)
+
+    if not object or object:ToString() ~= "NPCPuppet" or object:GetRecordID().hash ~= 966543935 then
+        drawSmuggleWindow = false
+    end
+end
+
+function SmugglerManager.CheckSmuggler(object, smugglingCost)
+    print("smuggle check")
+    if object:ToString() == "NPCPuppet" and object:GetRecordID().hash == 966543935 then
+
+        if drawSmuggleWindow then
+            ts:RemoveItem(Game.GetPlayer(), moneyID, 50000)
+            tp:Teleport(Game.GetPlayer(), ToVector4{ x = -899.2886, y = 1319.1085, z = 5.8175964, w = 1.0 } , EulerAngles.new(0,0,-45.378467559814))
+            drawSmuggleWindow = false
+        else
+            print("reached")
+            moneyQuantity = ts:GetItemQuantity(Game.GetPlayer(), moneyID)
+
+            if moneyQuantity >= smugglingCost then 
+                print("can smuggle")
+                drawSmuggleWindow = true
+            else
+                as:AddLog("Smuggling: Not enough eddies for smuggling. You need 50.000")
+            end
+        end
+
+    end
+
+end
+
+return SmugglerManager
